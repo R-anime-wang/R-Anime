e53f0852a8bb193e4e6ed428db6e730c70cbe698
Rahat <r.anime2025r@gmail.com>
2025-06-26 15:26:39 +0600

    Add files via upload

--- id.php
+++ id.php
@@ -1,12 +1,8 @@
-<?php 
+<?php
 require('../_config.php');
 $parts=parse_url($_SERVER['REQUEST_URI']); 
 $page_url=explode('/', $parts['path']);
-$id = $page_url[count($page_url)-1];
-//$id = "action";
-$genre = str_replace("+", "-", $id);
-$id = str_replace("+", " ", $id);
-$id = ucfirst($id);
+$url = $page_url[count($page_url)-1];
 $page = $_GET['page']; 
 if ($page == ""){
     $page = 1;
@@ -16,11 +12,11 @@ if ($page == ""){
 <html prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-    <title><?=$id?> on <?=$websiteTitle?></title>
+    <title>Anime List on <?=$websiteTitle?></title>
     
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="title" content="<?=$id?> on <?=$websiteTitle?>">
-    <meta name="description" content="Popular Anime in HD with No Ads. Watch anime online">
+    <meta name="title" content="Anime List on <?=$websiteTitle?>">
+    <meta name="description" content="Anime List in HD with No Ads. Watch anime online">
     <meta name="keywords" content="<?=$websiteTitle?>, watch anime online, free anime, anime stream, anime hd, english sub, kissanime, gogoanime, animeultima, 9anime, 123animes, <?=$websiteTitle?>, vidstreaming, gogo-stream, animekisa, zoro.to, gogoanime.run, animefrenzy, animekisa">
     <meta name="charset" content="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
@@ -28,8 +24,8 @@ if ($page == ""){
     <meta name="googlebot" content="index, follow">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta http-equiv="Content-Language" content="en">
-    <meta property="og:title" content="<?=$id?> on <?=$websiteTitle?>">
-    <meta property="og:description" content="<?=$id?> on <?=$websiteTitle?> in HD with No Ads. Watch anime online">
+    <meta property="og:title" content="Anime List on <?=$websiteTitle?>">
+    <meta property="og:description" content="Anime List on <?=$websiteTitle?> in HD with No Ads. Watch anime online">
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="<?=$websiteTitle?>">
@@ -63,8 +59,10 @@ if ($page == ""){
         }, 500);
     </script>
     <noscript>
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+        <link rel="stylesheet" type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" />
     </noscript>
     <script></script>
 </head>
@@ -72,7 +70,7 @@ if ($page == ""){
 <body data-page="page_anime">
     <div id="sidebar_menu_bg"></div>
     <div id="wrapper" data-page="page_home">
-        <?php include('../_php/header.php'); ?>
+        <?php include('../_php/header.php')?>
         <div class="clearfix"></div>
         <div id="main-wrapper">
             <div class="container">
@@ -80,7 +78,14 @@ if ($page == ""){
                     <section class="block_area block_area_category">
                         <div class="block_area-header">
                             <div class="float-left bah-heading mr-4">
-                                <h2 class="cat-heading">Genre: <?=$id?></h2>
+                                <h2 class="cat-heading">Anime List - <?=$url?></h2>
+                            </div>
+                            <div class="float-right bah-result">
+                                <div class="cmb-item">
+                                    <div class="nl-item">
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
                             </div>
                             <div class="clearfix"></div>
                         </div>
@@ -89,52 +94,31 @@ if ($page == ""){
                                 <div class="film_list-wrap">
 
                                 <?php 
-                                $json = file_get_contents("$api/genre/$genre?page=$page");
+                                $json = file_get_contents("$api/animeListAZ?aph=$url&page=$page");
                                 $json = json_decode($json, true);
-                                foreach($json as $key => $genreFetch) { ?>
+                                foreach($json as $key => $az) { ?>
                                     <div class="flw-item">
-                                        <div class="film-poster">
-                                            <div class="tick ltr">
-                                                <div class="tick-item-<?php $str = $genreFetch['animeTitle'];
-                                                  $last_word_start = strrpos ( $str , " ") + 1;
-                                                  $last_word_end = strlen($str) - 1;
-                                                  $last_word = substr($str, $last_word_start, $last_word_end);
-                                                  if ($last_word == "(Dub)"){echo "dub";} else {echo "sub";}
-                                                ?>   tick-eps amp-algn"><?php $str = $genreFetch['animeTitle'];
-                                                $last_word_start = strrpos ( $str , " ") + 1;
-                                                $last_word_end = strlen($str) - 1;
-                                                $last_word = substr($str, $last_word_start, $last_word_end);
-                                                if ($last_word == "(Dub)"){echo "Dub";} else {echo "Sub";}
-                                              ?></div>
-                                            </div>
-                                            <div class="tick rtl">
-                                            </div>
-                                            <img class="film-poster-img lazyload"
-                                                data-src="<?=$genreFetch['animeImg']?>"
-                                                src="<?=$websiteUrl?>/files/images/no_poster.jpg"
-                                                alt="<?=$genreFetch['animeTitle']?>">
-                                            <a class="film-poster-ahref"
-                                                href="/anime/<?=$genreFetch['animeId']?>"
-                                                title="<?=$genreFetch['animeTitle']?>"
-                                                data-jname="<?=$genreFetch['animeTitle']?>"><i class="fas fa-play"></i></a>
-                                        </div>
                                         <div class="film-detail">
                                             <h3 class="film-name">
                                                 <a
-                                                    href="/anime/<?=$genreFetch['animeId']?>"
-                                                    title="<?=$genreFetch['animeTitle']?>"
-                                                    data-jname="<?=$genreFetch['animeTitle']?>"><?=$genreFetch['animeTitle']?></a>
+                                                    href="/anime/<?=$az['animeId']?>" title="<?=$az['animeTitle']?>"
+                                                    data-jname="<?=$az['animeTitle']?>"><?=$az['animeTitle']?></a>
                                             </h3>
-                                            <div class="description"></div>
                                             <div class="fd-infor">
-                                                <span class="fdi-item"><?=$genreFetch['releasedDate']?></span>
+                                                <span class="fdi-item"># <?php echo (136 * ($page - 1)) + $key+1 ?></span>
+                                                <span class="dot"></span>
+                                                <span class="fdi-item"><?php $str = $az['animeTitle'];
+                                                $last_word_start = strrpos ( $str , " ") + 1;
+                                                $last_word_end = strlen($str) - 1;
+                                                $last_word = substr($str, $last_word_start, $last_word_end);
+                                                if ($last_word == "(Dub)"){echo "DUBBED";} else {echo "SUBBED";}
+                                              ?></span>
                                             </div>
                                         </div>
                                         <div class="clearfix"></div>
                                     </div>
                                 <?php } ?>
-                                    
-
+                                  
                                 </div>
                                 <div class="clearfix"></div>
                                 <style>
@@ -164,9 +148,9 @@ if ($page == ""){
                                     <nav>
                                         <ul class="ulclear az-list">
                                         <?php 
-                                           $genreFetchPage = file_get_contents("$api/genrePage?genre=$genre&page=$page");
-                                           $genreFetchPage = json_decode($genreFetchPage, true); { ?>
-                                             <?=$genreFetchPage['pagination']; ?>
+                                           $AZPage = file_get_contents("$api/anime-AZ-page?aph=$url&page=$page");
+                                           $AZPage = json_decode($AZPage, true); { ?>
+                                             <?=$AZPage['pagination']; ?>
                                            <?php } ?>
                                         </ul>
                                     </nav>
@@ -193,9 +177,6 @@ if ($page == ""){
         <link rel="stylesheet" href="<?=$websiteUrl?>/files/css/jquery-ui.css">
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <script type="text/javascript" src="<?=$websiteUrl?>/files/js/function.js"></script>
-
-        <div style="display:none;">
-        </div>
     </div>
 </body>
 
